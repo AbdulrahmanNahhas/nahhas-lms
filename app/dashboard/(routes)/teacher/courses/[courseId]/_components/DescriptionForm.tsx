f4c42454ea82b2e3fc694527a07b05426f08383c
Abdulrahman Nahhas <nahhas909@gmail.com>
2023-11-28 20:27:41 +0300

    Fix Description Form

--- app/dashboard/(routes)/teacher/courses/[courseId]/_components/DescriptionForm.tsx
+++ app/dashboard/(routes)/teacher/courses/[courseId]/_components/DescriptionForm.tsx
@@ -15,11 +15,10 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { Course } from "@prisma/client";
 
 interface DescriptionFormProps {
-  initialData: {
-    description: string;
-  };
+  initialData: Course;
   courseId: string;
 }
 const formSchema = z.object({
@@ -35,7 +34,9 @@ const DescriptionForm = ({ initialData, courseId }: DescriptionFormProps) => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: {
+      description: initialData?.description || ""
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
