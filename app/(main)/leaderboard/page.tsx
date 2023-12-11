a548753c5b4b433ec9331a775256856d03f99e84
Abdulrahman Nahhas <nahhas909@gmail.com>
2023-12-11 13:50:56 +0300

    coming soon

---
+++ app/(main)/leaderboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-[calc(100vh-80px)] md:min-h-screen flex items-center justify-center flex-col'>
+      <h1 className='font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-muted-foreground'>
+        Coming Soon
+      </h1>
+      <p className='text-muted-foreground md:text-lg mt-2'>Points system in development...</p>
+    </div>
+  )
+}
+
+export default page
