93cf336d7b94de695788f9d7000f23cca66ec9ce
Abdulrahman Nahhas <nahhas909@gmail.com>
2024-02-22 15:43:28 +0300

    Improving Text Editor & Preview

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,63 +2,18 @@
 
 import MDEditor, { ContextStore } from '@uiw/react-md-editor';
 
-// import dynamic from "next/dynamic";
-// import React, { useMemo } from "react";
-
-// import "react-quill/dist/quill.snow.css";
-
 interface EditorProps {
-  // onChange: (value: string) => void;
   onChange: (value?: string, event?: React.ChangeEvent<HTMLTextAreaElement>, state?: ContextStore) => void
   value: string;
 }
 
 const Editor = ({ onChange, value }: EditorProps) => {
-  // const ReactQuill = useMemo(
-  //   () => dynamic(() => import("react-quill"), { ssr: false }),
-  //   []
-  // );
-
   return (
-    // <ReactQuill
-    //   theme="snow"
-    //   value={value}
-    //   onChange={onChange}
-    //   modules={{
-    //     toolbar: [
-    //       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-    //       ["bold", "italic", "underline", "strike"],
-    //       // [{ 'script': 'sub'}, { 'script': 'super' }],
-    //       // [{ 'align': [] }],
-    //       [
-    //         { list: "ordered" },
-    //         { list: "bullet" },
-    //         { indent: "-1" },
-    //         { indent: "+1" },
-    //       ],
-    //       // [{ 'direction': 'rtl' }],
-    //       ["link", "image"],
-    //     ],
-    //   }}
-    //   formats={[
-    //     "header",
-    //     "bold",
-    //     "italic",
-    //     "underline",
-    //     "strike",
-    //     "blockquote",
-    //     "list",
-    //     "bullet",
-    //     "indent",
-    //     "link",
-    //     "image",
-    //   ]}
-    // />
     <MDEditor
-    className='min-h-[500px]'
+      className='min-h-[500px]'
       value={value}
       onChange={onChange}
-      previewOptions={["edit", "preview"]}
+      previewOptions={["edit"]}
       preview="edit"
     />
   );
--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,22 +1,17 @@
 "use client"
 
-import dynamic from 'next/dynamic'
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import 'react-quill/dist/quill.bubble.css';
+// import 'react-quill/dist/quill.bubble.css';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 
 interface PreviewProps {
   value: string;
-  className?: string;
 }
 
-const Preview = ({value, className}: PreviewProps) => {
-  // const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), {ssr: false}), []);
-
+const Preview = ({value}: PreviewProps) => {
   return (
     <MarkdownPreview source={value} className='p-4 rounded-xl border mt-4' />
-    // <ReactQuill theme="bubble" value={value} readOnly />
   )
 }
 
